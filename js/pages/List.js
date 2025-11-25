1b573c923e40584589be55c21be4f70bc45808cf
fayodayo <165217859+fayodayo@users.noreply.github.com>
2025-11-25 22:48:24 +0000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -38,8 +38,8 @@ export default {
                         'platinum': (i >= 82) && (i <= 101) , 
                         'amber': (i >= 102) && (i <= 119) , 
                         'gold': (i >= 120) && (i <= 147) , 
-                        'silver': (i >= 148) && (i <= 164) , 
-                        'bronze': (i >= 165) && (i <= 189) , 
+                        'silver': (i >= 148) && (i <= 163) , 
+                        'bronze': (i >= 165) && (i <= 188) , 
                         'beginner': (i >= 190) && (i <= 202) , 
                         'wood': (i >= 203)}">
                             <button @click="selected = i">
