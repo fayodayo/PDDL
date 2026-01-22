cac1ec6bafae4115bc3b538f45e54ed4563767d1
fayodayo <165217859+fayodayo@users.noreply.github.com>
2026-01-22 15:09:35 +0000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -32,16 +32,16 @@ export default {
                         'pearl': i <= 2 , 
                         'diamond': (i >= 2) && (i <= 10) , 
                         'ruby': (i >= 11) && (i <= 27) , 
-                        'emerald': (i >= 28) && (i <= 43) , 
-                        'jade': (i >= 44) && (i <= 64) , 
-                        'sapphire': (i >= 65) && (i <= 92) , 
-                        'platinum': (i >= 93) && (i <= 115) , 
-                        'amber': (i >= 116) && (i <= 136) , 
-                        'gold': (i >= 137) && (i <= 162) , 
-                        'silver': (i >= 163) && (i <= 178) , 
-                        'bronze': (i >= 179) && (i <= 203) , 
-                        'beginner': (i >= 204) && (i <= 217) , 
-                        'wood': (i >= 218)}">
+                        'emerald': (i >= 28) && (i <= 44) , 
+                        'jade': (i >= 45) && (i <= 65) , 
+                        'sapphire': (i >= 66) && (i <= 93) , 
+                        'platinum': (i >= 94) && (i <= 116) , 
+                        'amber': (i >= 117) && (i <= 137) , 
+                        'gold': (i >= 138) && (i <= 163) , 
+                        'silver': (i >= 164) && (i <= 179) , 
+                        'bronze': (i >= 180) && (i <= 204) , 
+                        'beginner': (i >= 205) && (i <= 218) , 
+                        'wood': (i >= 219)}">
                             <button @click="selected = i">
                                 <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
                             </button>
