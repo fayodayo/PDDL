aadc5e5595303c04219a341ecad3d665f0d3f45a
fayodayo <165217859+fayodayo@users.noreply.github.com>
2026-06-19 15:34:09 +0100

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -64,9 +64,9 @@ export default {
                         'titanium': (score.rank >= 136) && (score.rank <= 149) ,
                         'platinum': (score.rank >= 150) && (score.rank <= 169) , 
                         'amber': (score.rank >= 170) && (score.rank <= 194) , 
-                        'gold': (score.rank >= 195) && (score.rank <= 218) , 
-                        'silver': (score.rank >= 219) && (score.rank <= 234) , 
-                        'bronze': (score.rank >= 235) && (score.rank <= 259) , 
+                        'gold': (score.rank >= 195) && (score.rank <= 217) , 
+                        'silver': (score.rank >= 218) && (score.rank <= 233) , 
+                        'bronze': (score.rank >= 234) && (score.rank <= 259) , 
                         'beginner': (score.rank >= 260) && (score.rank <= 273) , 
                         'wood': (score.rank >= 274)}"  target="_blank" :href="score.link">{{ score.level }}</a>
                                 </td>
@@ -94,9 +94,9 @@ export default {
                         'titanium': (score.rank >= 136) && (score.rank <= 149) ,
                         'platinum': (score.rank >= 150) && (score.rank <= 169) , 
                         'amber': (score.rank >= 170) && (score.rank <= 194) , 
-                        'gold': (score.rank >= 195) && (score.rank <= 218) , 
-                        'silver': (score.rank >= 219) && (score.rank <= 234) , 
-                        'bronze': (score.rank >= 235) && (score.rank <= 259) , 
+                        'gold': (score.rank >= 195) && (score.rank <= 217) , 
+                        'silver': (score.rank >= 218) && (score.rank <= 233) , 
+                        'bronze': (score.rank >= 234) && (score.rank <= 259) , 
                         'beginner': (score.rank >= 260) && (score.rank <= 273) , 
                         'wood': (score.rank >= 274)}"  target="_blank" :href="score.link">{{ score.level }}</a>
                                 </td>
@@ -124,9 +124,9 @@ export default {
                         'titanium': (score.rank >= 136) && (score.rank <= 149) ,
                         'platinum': (score.rank >= 150) && (score.rank <= 169) , 
                         'amber': (score.rank >= 170) && (score.rank <= 194) , 
-                        'gold': (score.rank >= 195) && (score.rank <= 218) , 
-                        'silver': (score.rank >= 219) && (score.rank <= 234) , 
-                        'bronze': (score.rank >= 235) && (score.rank <= 259) , 
+                        'gold': (score.rank >= 195) && (score.rank <= 217) , 
+                        'silver': (score.rank >= 218) && (score.rank <= 233) , 
+                        'bronze': (score.rank >= 234) && (score.rank <= 259) , 
                         'beginner': (score.rank >= 260) && (score.rank <= 273) , 
                         'wood': (score.rank >= 274)}" target="_blank" :href="score.link">{{ score.percent }}% {{ score.level }}</a>
                                 </td>
