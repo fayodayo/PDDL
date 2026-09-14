dabafa7ca0308e7fbde2be99043a3fd25ad476f9
fayodayo <165217859+fayodayo@users.noreply.github.com>
2026-09-14 20:02:21 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-const maxRank = 420; // update as needed
+const maxRank = 421; // update as needed
 
 let basePoints =
     0.1 + (250 - 0.1) * Math.pow((maxRank - rank) / (maxRank - 1), 3);
