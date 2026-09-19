1ba38008aa1011db453789e9ba1b87b3f5a0aaed
fayodayo <165217859+fayodayo@users.noreply.github.com>
2026-09-19 16:33:12 +0100

    Adjust ranges for various item types in List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -34,17 +34,17 @@ export default {
                         'diamond': (i >= 9) && (i <= 26) , 
                         'ruby': (i >= 27) && (i <= 62) , 
                         'emerald': (i >= 63) && (i <= 97) , 
-                        'jade': (i >= 98) && (i <= 123) , 
-                        'osmium': (i >= 124) && (i <= 156) ,
-                        'sapphire': (i >= 157) && (i <= 174) , 
-                        'titanium': (i >= 175) && (i <= 199) ,
-                        'platinum': (i >= 200) && (i <= 222) , 
-                        'amber': (i >= 223) && (i <= 259) , 
-                        'gold': (i >= 260) && (i <= 292) , 
-                        'silver': (i >= 293) && (i <= 318) ,  
-                        'bronze': (i >= 319) && (i <= 349) , 
-                        'beginner': (i >= 350) && (i <= 381) , 
-                        'wood': (i >= 382)}">
+                        'jade': (i >= 98) && (i <= 124) , 
+                        'osmium': (i >= 125) && (i <= 157) ,
+                        'sapphire': (i >= 158) && (i <= 175) , 
+                        'titanium': (i >= 176) && (i <= 200) ,
+                        'platinum': (i >= 201) && (i <= 223) , 
+                        'amber': (i >= 224) && (i <= 260) , 
+                        'gold': (i >= 261) && (i <= 295) , 
+                        'silver': (i >= 296) && (i <= 321) ,  
+                        'bronze': (i >= 322) && (i <= 352) , 
+                        'beginner': (i >= 353) && (i <= 385) , 
+                        'wood': (i >= 386)}">
                             <button @click="selected = i">
                                 <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
                             </button>
