0f7eefc8b3c77ffd031a7f7fe548d313e5551fdd
fayodayo <165217859+fayodayo@users.noreply.github.com>
2026-09-19 16:34:02 +0100

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -58,17 +58,17 @@ export default {
                         'diamond': (score.rank >= 10) && (score.rank <= 27) , 
                         'ruby': (score.rank >= 28) && (score.rank <= 63) , 
                         'emerald': (score.rank >= 64) && (score.rank <= 98) , 
-                        'jade': (score.rank >= 99) && (score.rank <= 124) , 
-                        'osmium': (score.rank >= 125) && (score.rank <= 157) ,
-                        'sapphire': (score.rank >= 158) && (score.rank <= 175) , 
-                        'titanium': (score.rank >= 176) && (score.rank <= 200) ,
-                        'platinum': (score.rank >= 201) && (score.rank <= 223) , 
-                        'amber': (score.rank >= 224) && (score.rank <= 260) , 
-                        'gold': (score.rank >= 261) && (score.rank <= 293) , 
-                        'silver': (score.rank >= 294) && (score.rank <= 320) , 
-                        'bronze': (score.rank >= 321) && (score.rank <= 350) , 
-                        'beginner': (score.rank >= 351) && (score.rank <= 382) , 
-                        'wood': (score.rank >= 383)}"  target="_blank" :href="score.link">{{ score.level }}</a>
+                        'jade': (score.rank >= 99) && (score.rank <= 125) , 
+                        'osmium': (score.rank >= 126) && (score.rank <= 158) ,
+                        'sapphire': (score.rank >= 159) && (score.rank <= 176) , 
+                        'titanium': (score.rank >= 177) && (score.rank <= 201) ,
+                        'platinum': (score.rank >= 202) && (score.rank <= 224) , 
+                        'amber': (score.rank >= 225) && (score.rank <= 261) , 
+                        'gold': (score.rank >= 262) && (score.rank <= 296) , 
+                        'silver': (score.rank >= 297) && (score.rank <= 323) , 
+                        'bronze': (score.rank >= 324) && (score.rank <= 353) , 
+                        'beginner': (score.rank >= 354) && (score.rank <= 386) , 
+                        'wood': (score.rank >= 387)}"  target="_blank" :href="score.link">{{ score.level }}</a>
                                 </td>
                                 <td class="score">
                                     <p>+{{ localize(score.score) }}</p>
